5de4ba7557d6f9ef088489dddfaabf912c555efe
cindyelopez28 <60763168+cindyelopez28@users.noreply.github.com>
2022-07-06 00:47:43 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+//
+
+const lives = document.querySelectorAll(".tries img"); 
+const ul = document.getElementById("phrase").firstElementChild;
+const startGameButton = document.getElementById("btn__reset"); 
+let game;
+
+//Create a new game object when start button is clicked and show display keys on screen
+startGameButton.addEventListener("click", () =>{
+    game = new Game();
+    game.startGame();
+})
+
+const keyboardKeys = document.querySelectorAll('.key');
+
+keyboardKeys.forEach(button => {
+    button.addEventListener('click', () =>{
+        game.handleInteraction(button)
+        console.log(button);
+        }
+    )
+})
